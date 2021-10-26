9796c75326bc234b7f752bafad26c3c62e5d3194
SvetlinNikolov <daxtera@abv.bg>
2021-10-26 22:16:43 +0300

    Add  flavour variations

---
+++ src/Services/ShishaProject.Services.Data/Models/Dtos/FlavourVariation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShishaProject.Services.Data.Models.Dtos
+{
+    public class FlavourVariation
+    {
+        public int id { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public string weight { get; set; }
+        public int price { get; set; }
+        public int flavour_id { get; set; }
+    }
+}
