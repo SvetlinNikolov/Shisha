5db853726e2a1b18267c5ab46c4adec3aa675bea
SvetlinNikolov <daxteranikolov@gmail.com>
2021-12-01 18:12:52 +0200

    Logger works with relative path now

--- src/Services/ShishaProject.Services.Data/Models/Dtos/FlavourVariation.cs
+++ src/Services/ShishaProject.Services.Data/Models/Dtos/FlavourVariation.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ShishaProject.Services.Data.Models.Dtos
+﻿namespace ShishaProject.Services.Data.Models.Dtos
 {
+    using System;
+
+    using Newtonsoft.Json;
+
     public class FlavourVariation
     {
-        public int id { get; set; }
-        public DateTime created_at { get; set; }
-        public DateTime updated_at { get; set; }
-        public string weight { get; set; }
-        public int price { get; set; }
-        public int flavour_id { get; set; }
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("weight")]
+        public string Weight { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("flavour_id")]
+        public int FlavourId { get; set; }
     }
 }
